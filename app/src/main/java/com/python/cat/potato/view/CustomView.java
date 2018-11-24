f0deb0468f967810f258497c538e023a14eaa72e
pythonCat <pythoncat@qq.com>
2018-11-24 15:11:40 +0800

    set path filletype :FillType.WINDING

--- app/src/main/java/com/python/cat/potato/view/CustomView.java
+++ app/src/main/java/com/python/cat/potato/view/CustomView.java
@@ -7,6 +7,7 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.Region;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -37,16 +38,26 @@ public class CustomView extends View {
 
     private void init() {
         paint = new Paint();
+        paint.setColor(Color.GRAY);
         paint.setStrokeWidth(5);
         paint.setStyle(Paint.Style.FILL);
         paint.setTextSize(50);
         path = new Path();
-
+        RectF rectF = new RectF(100, 100, 700, 700);
+        path.addRect(rectF,Path.Direction.CW);
+        RectF oval = new RectF(400, 400, 1000, 1000);
+//        path.addArc(oval,0,360); // 强制 CW，不能指定！
+        path.addCircle(700,700,300,Path.Direction.CW);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        paint.setStyle(Paint.Style.STROKE);
+        paint.setStyle(Paint.Style.FILL);
+        //  [前提，方向相同]都是 CW,或都是 CCW
+//        path.setFillType(Path.FillType.WINDING); // 理论上是并集
+        path.setFillType(Path.FillType.EVEN_ODD); // XOR
+        canvas.drawPath(path,paint);
+
     }
 }
