86ce1020a8d745fb46ab67a00b7bfcaca5ef3682
pythonCat <pythoncat@qq.com>
2018-11-24 14:35:34 +0800

    reset custom view

--- app/src/main/java/com/python/cat/potato/view/CustomView.java
+++ app/src/main/java/com/python/cat/potato/view/CustomView.java
@@ -14,10 +14,7 @@ import android.view.View;
 
 public class CustomView extends View {
 
-    private int mX;
-    private int mY;
     private Paint paint;
-    private Rect rect;
     private Path path;
 
     public CustomView(Context context) {
@@ -43,8 +40,6 @@ public class CustomView extends View {
         paint.setStrokeWidth(5);
         paint.setStyle(Paint.Style.FILL);
         paint.setTextSize(50);
-        rect = new Rect();
-        rect.set(100, 100, 400, 400);
         path = new Path();
 
     }
@@ -52,54 +47,6 @@ public class CustomView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        if (rect.contains(mX, mY)) {
-            paint.setColor(Color.RED);
-        } else {
-            paint.setColor(Color.BLUE);
-        }
         paint.setStyle(Paint.Style.STROKE);
-        path.moveTo(49, 49);
-        // forceMoveTo: 强制不连接之前的路径
-        path.arcTo(new RectF(rect), 45, 90, true);
-
-        path.addArc(500, 500, 600, 600, -90, -180);
-        canvas.drawPath(path, paint);
-
-        RectF oval = new RectF(100, 700, 700, 1000);
-        path.reset();
-        path.addOval(oval, Path.Direction.CW);
-        String text = "我是比亚迪车主，你是比亚迪";
-        canvas.drawTextOnPath(text,path,0,0,paint);
-        canvas.drawPath(path, paint);
-    }
-
-    @Override
-    public boolean performClick() {
-        return super.performClick();
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                mX = (int) event.getX();
-                mY = (int) event.getY();
-                invalidate();
-                return true;
-            case MotionEvent.ACTION_MOVE:
-                mX = (int) event.getX();
-                mY = (int) event.getY();
-                invalidate();
-                break;
-            case MotionEvent.ACTION_UP:
-                performClick();
-            case MotionEvent.ACTION_CANCEL:
-                mX = -1;
-                mY = -1;
-                postInvalidate();
-                break;
-
-        }
-        return false;
     }
 }
