98e9b7fb1e52fdf46e71abee06bf64c493c31616
pythonCat <pythoncat@qq.com>
2018-11-26 22:04:34 +0800

    不规则按钮实现雏形[region 的使用]

--- app/src/main/java/com/python/cat/potato/fragment/ViewFragment.java
+++ app/src/main/java/com/python/cat/potato/fragment/ViewFragment.java
@@ -3,13 +3,17 @@ package com.python.cat.potato.fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.apkfuns.logutils.LogUtils;
 import com.python.cat.potato.R;
 import com.python.cat.potato.base.DrawerFragment;
+import com.python.cat.potato.utils.ToastHelper;
+import com.python.cat.potato.view.CustomView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -44,6 +48,20 @@ public class ViewFragment extends DrawerFragment {
         return inflater.inflate(R.layout.fragment_view, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        CustomView cv = view.findViewById(R.id.view_fragment_custom_view);
+        cv.setOnAreaClickListener((v, area) -> {
+            ToastHelper.show(v.getContext(), area.name());
+            LogUtils.w("area click --" + area.name());
+        });
+
+        cv.setOnClickListener(v -> {
+            ToastHelper.show(v.getContext(), "click....");
+            LogUtils.w("click...");
+        });
+    }
 
     @Override
     public void onAttach(Context context) {
--- app/src/main/java/com/python/cat/potato/view/CustomView.java
+++ app/src/main/java/com/python/cat/potato/view/CustomView.java
@@ -1,26 +1,49 @@
 package com.python.cat.potato.view;
 
 import android.content.Context;
-import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.EmbossMaskFilter;
-import android.graphics.MaskFilter;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.Region;
+import android.graphics.RegionIterator;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+
+import com.apkfuns.logutils.LogUtils;
 
 public class CustomView extends View {
 
 
-    private Paint textPaint;
-    private String text;
-    private Paint linePaint;
-    private Path path;
-    private int mH;
+    private Region centerRegion;
+    private Region leftTopRegion;
+    private Region leftBottomRegion;
+    private Region rightTopRegion;
+    private Region rightBottomRegion;
+    private Rect tempRect;
     private int mW;
+    private int mH;
+    private int centerX;
+    private int centerY;
+    private Rect outer;
+    private RectF ovalOuter;
+    private RectF ovalInner;
+    private Path path;
+    private Region clip;
+    private int mX;
+    private int mY;
+    private Paint centerPaint;
+    private Paint leftTopPaint;
+    private Paint leftBottomPaint;
+    private Paint rightTopPaint;
+    private Paint rightBottomPaint;
+
+    private int accountColor = Color.RED;
+    private OnAreaClickListener mAreaListener;
 
     public CustomView(Context context) {
         this(context, null);
@@ -43,44 +66,297 @@ public class CustomView extends View {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        this.mH = h;
         this.mW = w;
+        this.mH = h;
+        this.centerX = mW / 2;
+        this.centerY = mH / 2;
+        setOuter();
+        setOvalOuter();
+        setOvalInner();
+
+        setCenterRegion();
+
+        setLeftTopRegion();
+        setRightTopRegion();
+        setLeftBottomRegion();
+        setRightBottomRegion();
+    }
+
+    private void setRightBottomRegion() {
+        path.reset();
+        path.rewind();
+        RectF rectF = new RectF();
+        float left = centerX + mW / 2 * 0.1f;
+        float top = centerY + mH / 2 * 0.1f;
+        float right = centerX + mW / 2;
+        float bottom = centerY + mH / 2;
+        rectF.set(left, top, right, bottom);
+
+        path.addRect(rectF, Path.Direction.CW);
+
+        Path another = new Path();
+        another.addOval(ovalOuter, Path.Direction.CW);
+        path.op(another, Path.Op.DIFFERENCE);
+        clip.set(outer);
+        rightBottomRegion.setPath(path, clip);
+
+        LogUtils.d(rectF);
+    }
+
+    private void setLeftBottomRegion() {
+        path.reset();
+        path.rewind();
+        RectF rectF = new RectF();
+        float left = outer.left;
+        float top = centerY + mH / 2 * 0.1f;
+        float right = centerX - mW / 2 * 0.1f;
+        float bottom = centerY + mH / 2;
+        rectF.set(left, top, right, bottom);
+
+        path.addRect(rectF, Path.Direction.CW);
+
+        Path another = new Path();
+        another.addOval(ovalOuter, Path.Direction.CW);
+        path.op(another, Path.Op.DIFFERENCE);
+        clip.set(outer);
+        leftBottomRegion.setPath(path, clip);
+
+        LogUtils.d(rectF);
+    }
+
+    private void setLeftTopRegion() {
+        path.reset();
+        path.rewind();
+
+        RectF rectF = new RectF();
+        float left = outer.left;
+        float top = outer.top;
+        float right = centerX - mW / 2 * 0.1f;
+        float bottom = centerY - mH / 2 * 0.1f;
+        rectF.set(left, top, right, bottom);
+
+        path.addRect(rectF, Path.Direction.CW);
+
+        Path another = new Path();
+        another.addOval(ovalOuter, Path.Direction.CW);
+        path.op(another, Path.Op.DIFFERENCE);
+        clip.set(outer);
+        leftTopRegion.setPath(path, clip);
+    }
+
+    private void setRightTopRegion() {
+        path.reset();
+        path.rewind();
+
+        RectF rectF = new RectF();
+        float left = centerX + mW / 2 * 0.1f;
+        float top = outer.top;
+        float right = centerX + mW / 2;
+        float bottom = centerY - mH / 2 * 0.1f;
+        rectF.set(left, top, right, bottom);
+
+        path.addRect(rectF, Path.Direction.CW);
+        Path another = new Path();
+        another.addOval(ovalOuter, Path.Direction.CW);
+        path.op(another, Path.Op.DIFFERENCE);
+        clip.set(outer);
+        rightTopRegion.setPath(path, clip);
+    }
+
+    private void setCenterRegion() {
+        path.reset();
+        path.rewind();
+        path.addOval(ovalInner, Path.Direction.CW);
+        clip.set(outer);
+        centerRegion.setPath(path, clip);
+    }
+
+
+    private void setOvalInner() {
+        float left = centerX - mW / 2 * 0.7f;
+        float right = centerX + mW / 2 * 0.7f;
+        float top = centerY - mH / 2 * 0.7f;
+        float bottom = centerY + mH / 2 * 0.7f;
+        ovalInner.set(left, top, right, bottom);
+        LogUtils.v(ovalOuter);
+    }
+
+    private void setOvalOuter() {
+        float left = centerX - mW / 2 * 0.8f;
+        float right = centerX + mW / 2 * 0.8f;
+        float top = centerY - mH / 2 * 0.8f;
+        float bottom = centerY + mH / 2 * 0.8f;
+        ovalOuter.set(left, top, right, bottom);
+        LogUtils.v(ovalOuter);
+    }
+
+    private void setOuter() {
+        int left = (int) (centerX - mW / 2 * 0.9f);
+        int right = (int) (centerX + mW / 2 * 0.9f);
+        int top = (int) (centerY - mH / 2 * 0.9f);
+        int bottom = (int) (centerY + mH / 2 * 0.9f);
+        outer.set(left, top, right, bottom);
+        LogUtils.v(outer);
     }
 
     private void init() {
-        linePaint = new Paint();
-        linePaint.setColor(Color.RED);
-        linePaint.setAlpha(100);
-        linePaint.setStrokeWidth(2);
-        linePaint.setStyle(Paint.Style.STROKE);
+        initPaint();
 
-        textPaint = new Paint();
-        textPaint.setAntiAlias(true); // 抗锯齿
-        textPaint.setTextSize(80);
-        textPaint.setStyle(Paint.Style.FILL);
-        textPaint.setStrokeWidth(2);
-        text = "明明如月，何时可掇？";
-
-        textPaint.setTextAlign(Paint.Align.LEFT);
-        textPaint.setFakeBoldText(true); // 字体加粗
-        textPaint.setUnderlineText(true); // 在 paint.drawTextOnPath 时无效
-        textPaint.setStrikeThruText(true); // 在 paint.drawTextOnPath 时无效
-        textPaint.setTextSkewX(-0.25f); // 错切
         path = new Path();
+
+        tempRect = new Rect();
+        clip = new Region();
+
+        centerRegion = new Region();
+        leftTopRegion = new Region();
+        leftBottomRegion = new Region();
+        rightTopRegion = new Region();
+        rightBottomRegion = new Region();
+        outer = new Rect();
+        ovalOuter = new RectF();
+        ovalInner = new RectF();
     }
+
+    private void initPaint() {
+        centerPaint = new Paint();
+        leftTopPaint = new Paint();
+        leftBottomPaint = new Paint();
+        rightTopPaint = new Paint();
+        rightBottomPaint = new Paint();
+    }
+
+    private void updatePaint(Area area) {
+        switch (area) {
+            case NONE:
+                centerPaint.setColor(Color.BLACK);
+                leftBottomPaint.setColor(Color.BLACK);
+                leftTopPaint.setColor(Color.BLACK);
+                rightTopPaint.setColor(Color.BLACK);
+                rightBottomPaint.setColor(Color.BLACK);
+                break;
+            case LEFT_TOP:
+                centerPaint.setColor(Color.BLACK);
+                leftBottomPaint.setColor(Color.BLACK);
+                leftTopPaint.setColor(accountColor);
+                rightTopPaint.setColor(Color.BLACK);
+                rightBottomPaint.setColor(Color.BLACK);
+                break;
+            case LEFT_BOTTOM:
+                centerPaint.setColor(Color.BLACK);
+                leftBottomPaint.setColor(accountColor);
+                leftTopPaint.setColor(Color.BLACK);
+                rightTopPaint.setColor(Color.BLACK);
+                rightBottomPaint.setColor(Color.BLACK);
+                break;
+            case RIGHT_TOP:
+                centerPaint.setColor(Color.BLACK);
+                leftBottomPaint.setColor(Color.BLACK);
+                leftTopPaint.setColor(Color.BLACK);
+                rightTopPaint.setColor(accountColor);
+                rightBottomPaint.setColor(Color.BLACK);
+                break;
+            case RIGHT_BOTTOM:
+                centerPaint.setColor(Color.BLACK);
+                leftBottomPaint.setColor(Color.BLACK);
+                leftTopPaint.setColor(Color.BLACK);
+                rightTopPaint.setColor(Color.BLACK);
+                rightBottomPaint.setColor(accountColor);
+                break;
+            case CENTER_OVAL:
+                centerPaint.setColor(accountColor);
+                leftBottomPaint.setColor(Color.BLACK);
+                leftTopPaint.setColor(Color.BLACK);
+                rightTopPaint.setColor(Color.BLACK);
+                rightBottomPaint.setColor(Color.BLACK);
+                break;
+        }
+    }
+
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        drawRegion(canvas, centerRegion, tempRect, centerPaint);
+        drawRegion(canvas, leftTopRegion, tempRect, leftTopPaint);
+        drawRegion(canvas, leftBottomRegion, tempRect, leftBottomPaint);
+        drawRegion(canvas, rightTopRegion, tempRect, rightTopPaint);
+        drawRegion(canvas, rightBottomRegion, tempRect, rightBottomPaint);
+    }
 
-        // 先画线
-//        canvas.drawLine(100, 0, 100, 10000, linePaint); // y
-//        canvas.drawLine(0, 100, 10000, 100, linePaint); // x
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
 
-        path.moveTo(500, mH);
-        path.lineTo(500, 100);
-        canvas.drawPath(path, linePaint);
-        canvas.drawTextOnPath(text, path, 100, 100, textPaint);
-        canvas.drawText(text, 200, 200, textPaint);
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+                mX = (int) event.getX();
+                mY = (int) event.getY();
+                Area area = witchArea(mX, mY);
+                updatePaint(area);
+                break;
+            case MotionEvent.ACTION_UP:
+                performClick();
+                mX = -1;
+                mY = -1;
+                break;
+        }
+        invalidate();
+        return true;
+    }
+
+    @Override
+    public boolean performClick() {
+        Area area = witchArea(mX, mY);
+        if (mAreaListener != null) {
+            mAreaListener.click(this, area);
+        }
+//        return super.performClick();
+        return false; // 让 view.onClick 失效
+    }
+
+    private void drawRegion(Canvas canvas, Region region, Rect temp, Paint paint) {
+        RegionIterator iterator = new RegionIterator(region);
+        while (iterator.next(temp)) {
+            canvas.drawRect(temp, paint);
+        }
+    }
+
+    public enum Area {
+        NONE("none"),
+        LEFT_TOP("left_top"),
+        LEFT_BOTTOM("left_bottom"),
+        RIGHT_TOP("right_top"),
+        RIGHT_BOTTOM("right_bottom"),
+        CENTER_OVAL("center_oval");
+        private final String value;
+
+        Area(String none) {
+            this.value = none;
+        }
+    }
+
+    private Area witchArea(int x, int y) {
+        if (centerRegion.contains(x, y)) {
+            return Area.CENTER_OVAL;
+        } else if (leftTopRegion.contains(x, y)) {
+            return Area.LEFT_TOP;
+        } else if (leftBottomRegion.contains(x, y)) {
+            return Area.LEFT_BOTTOM;
+        } else if (rightTopRegion.contains(x, y)) {
+            return Area.RIGHT_TOP;
+        } else if (rightBottomRegion.contains(x, y)) {
+            return Area.RIGHT_BOTTOM;
+        } else {
+            return Area.NONE;
+        }
+    }
+
+
+    public interface OnAreaClickListener {
+        void click(View v, Area area);
+    }
+
+    public void setOnAreaClickListener(OnAreaClickListener listener) {
+        this.mAreaListener = listener;
     }
 }
