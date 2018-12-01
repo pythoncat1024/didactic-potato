33e0e9a5ff3bdd47b4e7b9c6a33d6c63ea585fc7
pythonCat <pythoncat@qq.com>
2018-12-01 21:53:01 +0800

    电话左右晃动效果
    https://gist.github.com/pythoncat1024/fdef316f8e5b024652801df62ad79738

--- app/src/main/java/com/python/cat/potato/fragment/ViewFragment.java
+++ app/src/main/java/com/python/cat/potato/fragment/ViewFragment.java
@@ -1,23 +1,25 @@
 package com.python.cat.potato.fragment;
 
+import android.animation.FloatEvaluator;
+import android.animation.IntEvaluator;
 import android.animation.Keyframe;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
 import android.animation.TypeEvaluator;
+import android.animation.ValueAnimator;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.AnticipateOvershootInterpolator;
+import android.view.animation.Animation;
 import android.view.animation.BounceInterpolator;
-import android.view.animation.OvershootInterpolator;
+import android.widget.ViewAnimator;
 
-import com.apkfuns.logutils.LogUtils;
 import com.python.cat.potato.R;
 import com.python.cat.potato.base.DrawerFragment;
 import com.python.cat.potato.view.CharTextView;
@@ -58,38 +60,37 @@ public class ViewFragment extends DrawerFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        CharTextView tvChar = view.findViewById(R.id.char_tv);
-        tvChar.setCharText('X');
-        PropertyValuesHolder charHolder = PropertyValuesHolder.ofObject("CharText",
-                new CharEvaluator(), 'A', 'Z');
-        charHolder.setKeyframes(Keyframe.ofObject(0, 'A'),
-                Keyframe.ofObject(0.1f, 'M'),
-                Keyframe.ofObject(0.5f, 'G'),
-                Keyframe.ofObject(0.8f, 'M'),
-                Keyframe.ofObject(1f, 'Z')
+        View callRing = view.findViewById(R.id.iv_call_ring);
+        PropertyValuesHolder rotationH = PropertyValuesHolder.ofFloat("rotation",
+                -60f, -10f, 0, 60f, 0f);
+        PropertyValuesHolder scaleXH = PropertyValuesHolder.ofFloat("scaleX",
+                1.0f, 0.7f, 1.2f, 1, 0.3f, 1f);
+        scaleXH.setKeyframes(Keyframe.ofFloat(0, 1f),
+                Keyframe.ofFloat(0.3f, 0.4f),
+                Keyframe.ofFloat(0.5f, 2f),
+                Keyframe.ofFloat(0.8f, 0.8f),
+                Keyframe.ofFloat(1f, 1f)
+        ); // 无效 --> setKeyframes 估计只对 ofObject 有效果
+        scaleXH.setEvaluator(new FloatEvaluator());
+        PropertyValuesHolder scaleYH = PropertyValuesHolder.ofFloat("scaleY",
+                1.0f, 0.7f, 1.2f, 1, 0.3f, 1f);
+        scaleYH.setKeyframes(Keyframe.ofFloat(0, 1f),
+                Keyframe.ofFloat(0.3f, 0.4f),
+                Keyframe.ofFloat(0.5f, 2f),
+                Keyframe.ofFloat(0.8f, 0.8f),
+                Keyframe.ofFloat(1f, 1f)
         );
-        PropertyValuesHolder translationY = PropertyValuesHolder.ofFloat("TranslationY", 0, 1000, 700, 1400, 0);
-        ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(tvChar, charHolder, translationY);
+        scaleYH.setEvaluator(new FloatEvaluator());
+        ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(callRing,
+                rotationH, scaleXH, scaleYH);
         view.findViewById(R.id.btn_start)
                 .setOnClickListener(v -> {
-                    animator.setDuration(6000);
-                    animator.setInterpolator(new BounceInterpolator());
+                    animator.setDuration(2000);
+//                    animator.setRepeatCount(Animation.INFINITE);
+                    animator.setRepeatMode(ValueAnimator.REVERSE);
                     animator.start();
                 });
 
-    }
-
-    private class CharEvaluator implements TypeEvaluator<Character> {
-
-        @Override
-        public Character evaluate(float fraction, Character startValue, Character endValue) {
-            int sta = startValue;
-            int end = endValue;
-            char c = (char) (sta + (end - sta) * fraction);
-//            LogUtils.e("char=== " + c);
-            return c;
-        }
     }
 
 
