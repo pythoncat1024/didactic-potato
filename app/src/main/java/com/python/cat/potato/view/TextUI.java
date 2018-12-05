2eaa8e67f83f27309ff53e888d44790ce27237c7
pythonCat <pythoncat@qq.com>
2018-12-05 23:19:49 +0800

    根据指定中线高度，居中绘制文字
    https://gist.github.com/pythoncat1024/8ca8bd4406decfcf5d9ffbb1786de528

--- app/src/main/java/com/python/cat/potato/view/TextUI.java
+++ app/src/main/java/com/python/cat/potato/view/TextUI.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -18,6 +19,8 @@ public class TextUI extends View {
     private Paint topPaint;
     private Paint bottomPaint;
     private Paint basePaint;
+    private Paint rectPaint;
+    private Paint midYPaint;
 
     public TextUI(Context context) {
         this(context, null);
@@ -62,6 +65,12 @@ public class TextUI extends View {
         descentPaint.setStrokeWidth(4);
         topPaint.setStrokeWidth(4);
         bottomPaint.setStrokeWidth(4);
+        rectPaint = new Paint();
+        rectPaint.setColor(Color.MAGENTA);
+        rectPaint.setStyle(Paint.Style.STROKE);
+
+        midYPaint = new Paint();
+        midYPaint.setColor(Color.CYAN);
     }
 
     @Override
@@ -70,15 +79,16 @@ public class TextUI extends View {
 
         int baseX = 100;
         int baseY = 300;
-//        LogUtils.w("before:==" + textPaint.getFontMetricsInt());
+        //        LogUtils.w("before:==" + textPaint.getFontMetricsInt());
         canvas.drawText("FfGgIiJj", baseX, baseY, textPaint);
-//        LogUtils.w("after:===" + textPaint.getFontMetricsInt());
+        //        LogUtils.w("after:===" + textPaint.getFontMetricsInt());
 
         // before 和 after 相同。说明这个值只跟 textPaint.setTextSize(size) 有关。
+        // 跟 baseLine 无关！
+
 
         drawSpecialLine(canvas, basePaint, baseX, baseY); // base line
         Paint.FontMetricsInt fontMetrics = textPaint.getFontMetricsInt();
-
 
         int topX = baseX;
         int topY = baseY + fontMetrics.top;
@@ -96,8 +106,55 @@ public class TextUI extends View {
         int bottomY = baseY + fontMetrics.bottom;
         drawSpecialLine(canvas, bottomPaint, bottomX, bottomY); // bottom line
 
+        //        ################################
+        //        ################################ 下面才是真实场景
+        //        ################################ 下面才是真实场景
+        //        ################################
+        int left = 100;
+        int top = 500;
+        int right = left + 837;
+        int bottom = top + 200;
+        canvas.drawRect(left, top, right, bottom, rectPaint);
+        // : 在矩形中间绘制文字！！！
+        int midY = top + (bottom - top) / 2;
+        LogUtils.d("midY===" + midY);
+        textPaint.setTextSize(100);
+        Paint.FontMetricsInt fontMetricsInt = textPaint.getFontMetricsInt();
+        int baseLineY = getBaseY(midY, fontMetricsInt);
+        canvas.drawText("我在矩形中间了吗？", left, baseLineY, textPaint);
+        canvas.drawLine(-1000, midY, 100 * 100, midY, midYPaint);
+
     }
 
+    /**
+     * <div style="color:blue">以指定的 y 坐标为中线，绘制文字 </div>
+     * <p>计算基线y坐标的方式如下：</p>
+     * <ul>
+     * <b>a.基线与四线格对应转换公式:</b>
+     * <li>int topY = baseY + fontMetrics.top;</li>
+     * <li>int ascentY = baseY + fontMetrics.ascent;</li>
+     * <li>int descentY = baseY + fontMetrics.descent;</li>
+     * <li>int bottomY = baseY + fontMetrics.bottom;</li>
+     * </ul>
+     * <ul>
+     * <b>b.文字所在区域高度计算公式：</b><br/>
+     * <i>高度实际计算公式是：int deltaH = bottomY - topY;
+     * <br/>由a(基线与四线格对应转换公式)可以将公式化简为：</i>
+     * <li>int deltaH = fontMetricsInt.bottom - fontMetricsInt.top; // ΔH</li>
+     * </ul>
+     *
+     * @param midY           指定的基准中线 y 坐标值
+     * @param fontMetricsInt Paint.FontMetrics
+     * @return baseY 对应的坐标值
+     */
+    private int getBaseY(int midY, @NonNull Paint.FontMetricsInt fontMetricsInt) {
+        int deltaH = fontMetricsInt.bottom - fontMetricsInt.top; // ΔH
+        int botY = midY + deltaH / 2;
+        int topY = midY - deltaH / 2;
+        // botY = baseY + bot;
+//        int baseY = botY - fontMetricsInt.bottom; // ok 效果相同
+        return topY - fontMetricsInt.top;
+    }
 
     private void drawSpecialLine(Canvas canvas, Paint paint, int startX, int startY) {
         canvas.drawLine(startX, startY, startX <= 0 ? 100 : startX * 100, startY, paint);
