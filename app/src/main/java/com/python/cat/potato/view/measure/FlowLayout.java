06215de3ec2914ffaa4e597bf73a6793fa2d744a
pythonCat <pythoncat@qq.com>
2018-12-23 01:13:40 +0800

    自定义流布局完成
    1. 在 onMeasure 的时候，遍历 child ,要特别注意记录的数据是当前行的，还是上一行的
    2. onLayout 同理，要时刻关注记录的数据是当前行的还是上一行的
    
    Signed-off-by: pythonCat <pythoncat@qq.com>

--- app/src/main/java/com/python/cat/potato/view/measure/FlowLayout.java
+++ app/src/main/java/com/python/cat/potato/view/measure/FlowLayout.java
@@ -5,19 +5,9 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.apkfuns.logutils.LogUtils;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class FlowLayout extends ViewGroup {
 
-    private List<View> lineViews = new ArrayList<>();
-
-    private List<int[]> childPosition = new ArrayList<>();
-
     private int maxWidth;
-    private int maxHeight;
 
     public FlowLayout(Context context) {
         this(context, null);
@@ -38,8 +28,10 @@ public class FlowLayout extends ViewGroup {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        LogUtils.d("size w=" + w + " ,,, h=" + h);
+//        LogUtils.d("size w=" + w + " ,,, h=" + h);
     }
+
+    int tempEnd = 0;
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
@@ -48,26 +40,17 @@ public class FlowLayout extends ViewGroup {
         int hs = MeasureSpec.getSize(heightMeasureSpec);
         int hm = MeasureSpec.getMode(heightMeasureSpec);
         this.maxWidth = ws;
-        this.maxHeight = ws;
-        LogUtils.i("default wh: " + ws + " , " + hs);
+        //        LogUtils.i("default wh: " + ws + " , " + hs);
         int count = getChildCount();
-        int lines = 0;
         int width = 0;
         int height = 0;
-        int preLineWidth = 0; // 预先的 行宽
-        int preLineHeight = 0; // 预先的 行宽
-        int sizeOfViewsInLine = 0;
-        boolean endLine; // 结束当前行：此时 进行 height += lineHeight
-        boolean needNext;
+        int lineWidth = 0; // 预先的 行宽
+        int lineHeight = 0; // 预先的 行宽
         for (int i = 0; i < count; i++) {
-            if (i == 3) {
-                LogUtils.w("333333");
-            }
             View child = getChildAt(i);
             if (child == null || child.getVisibility() == GONE) {
                 continue;
             }
-            sizeOfViewsInLine += 1;
 
             // 测量 child 宽高 --- start
             measureChildWithMargins(child, widthMeasureSpec, 0, heightMeasureSpec, 0);
@@ -78,33 +61,31 @@ public class FlowLayout extends ViewGroup {
             int itemW = measuredWidth + lp.leftMargin + lp.rightMargin;
             int itemH = measuredHeight + lp.topMargin + lp.bottomMargin;
 
-            preLineWidth += itemW;
-            needNext = sizeOfViewsInLine > 1 && preLineWidth + getPaddingRight() + getPaddingLeft() > ws;
-            if (needNext) {
-                endLine = true;
-                lines += 1;
-                sizeOfViewsInLine = 1;
-                preLineWidth = itemW;
-                preLineHeight = itemH;
+            if (lineWidth + itemW > ws - getPaddingLeft() - getPaddingRight()) {
+                // 换行 ,先记录换行之前的数据
+                width = Math.max(width, lineWidth); // 记录的是上一行的数据
+                height += lineHeight; // 记录的是上一行的数据
+
+                // 换行了
+                lineHeight = itemH;
+                lineWidth = itemW;
             } else {
-                endLine = false;
-                preLineHeight = Math.max(preLineHeight, itemH);
+                // 不换行
+                lineHeight = Math.max(lineHeight, itemH);
+                lineWidth += itemW;
             }
+
             if (i == count - 1) {
-                endLine = true;
+                // 最后一个 view ， 记录这一行的数据
+                width = Math.max(width, lineWidth); // 记录的是这一行的数据
+                height += lineHeight; // 记录这一行的数据
+
             }
-            width = Math.max(preLineWidth, width);
-            if (endLine) {
-                // 这时候 lineWidth 等于 新行的 第一个 child 的 宽度
-                height += preLineHeight;
-                LogUtils.wtf("well=== %s : %s %s", width, preLineWidth, lines);
-            }
-            LogUtils.wtf("child=%s , line=%s, lw=%s , h=%s ,w=%s", i, lines, preLineWidth, height, width);
+
         }
 
         int finalW = wm == MeasureSpec.EXACTLY ? ws : width + getPaddingLeft() + getPaddingRight();
         int finalH = hm == MeasureSpec.EXACTLY ? hs : height + getPaddingTop() + getPaddingBottom();
-        com.apkfuns.logutils.LogUtils.i("measure: " + finalW + " , " + finalH);
 //        setMeasuredDimension(ws, hs); // 设置成 fill_parent 了
         setMeasuredDimension(finalW, finalH);
     }
@@ -112,15 +93,56 @@ public class FlowLayout extends ViewGroup {
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
 
-    }
-/*
-l = width - itemW + lp.leftMargin;
-t = height - itemH + lp.topMargin;
-r = l + child.getMeasuredWidth();
-b = t + child.getMeasuredHeight();
-child.layout(l, t, r, b);
- */
+        int count = getChildCount();
+        int width = 0;
+        int height = 0;
+        int lineWidth = 0; // 预先的 行宽
+        int lineHeight = 0; // 预先的 行宽
+        for (int i = 0; i < count; i++) {
+            View child = getChildAt(i);
+            if (child == null || child.getVisibility() == GONE) {
+                continue;
+            }
 
+            // 测量 child 宽高 --- start
+            int measuredWidth = child.getMeasuredWidth();
+            int measuredHeight = child.getMeasuredHeight();
+            // 测量 child 宽高 --- end
+            MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
+            int itemW = measuredWidth + lp.leftMargin + lp.rightMargin;
+            int itemH = measuredHeight + lp.topMargin + lp.bottomMargin;
+
+            if (lineWidth + itemW > maxWidth - getPaddingLeft() - getPaddingRight()) {
+                // 换行 ,先记录换行之前的数据
+                width = Math.max(width, lineWidth); // 记录的是上一行的数据
+                height += lineHeight; // 记录的是上一行的数据
+
+                // 换行了
+                lineHeight = itemH;
+                lineWidth = itemW;
+            } else {
+                // 不换行
+                lineHeight = Math.max(lineHeight, itemH);
+                lineWidth += itemW;
+            }
+
+            if (i == count - 1) {
+                // 最后一个 view ， 记录这一行的数据
+                width = Math.max(width, lineWidth); // 记录的是这一行的数据
+                height += lineHeight; // 记录这一行的数据
+
+            }
+
+            l = lineWidth - itemW + lp.leftMargin + getPaddingLeft();
+            r = l + measuredWidth;
+            t = height + lp.topMargin + getPaddingTop();
+            if (i == count - 1) {
+                t = height - lineHeight + lp.topMargin;
+            }
+            b = t + measuredHeight;
+            child.layout(l, t, r, b);
+        }
+    }
 
     @Override
     protected MarginLayoutParams generateDefaultLayoutParams() {
